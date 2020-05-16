1b0f45b2b5287621f3566fad767346c91177da2d
MalyJPL <malyjpl@gmail.com>
2020-05-16 16:54:09 -0500

    hoja de vida

---
+++ resume.js
@@ -0,0 +1,50 @@
+const express = require('express'),
+      app = express(),
+      path = require('path'); //path es uno de los 34 móddulos de node
+      cookieParser = require('cookie-parser'),
+      cookieSession = require('cookie-session');
+
+      //nos pemrite ubicar los archivos de la carpeta public css&img
+      app.use(express.static(path.join(__dirname, "/public")));
+          //USO DE MIDDLEWARES
+    app.use(cookieParser());
+    app.use(cookieSession({secret: "I'm a secret"}));
+
+
+
+      // creamos las rutass de anuestr aaplicación
+      app.get('/', (peticion, respuesta)=>{
+        peticion.session.visitas || (peticion.session.visitas = 0);
+        let n = peticion.session.visitas++;
+        console.log(`Visita # ${n}`);
+        
+          respuesta.sendFile(`${__dirname}/views/index.html`);
+      });
+      
+
+      app.get('/acercademi', (peticion, respuesta)=>{
+          respuesta.sendFile(`${__dirname}/views/acercademi.html`);
+      });
+
+      app.get('/experiencia', (peticion, respuesta)=>{
+        respuesta.sendFile(`${__dirname}/views/experiencia.html`);
+    });
+      
+    app.get('/educacion', (peticion, respuesta)=>{
+        respuesta.sendFile(`${__dirname}/views/educacion.html`);
+    });
+
+    app.get('/idiomas', (peticion, respuesta)=>{
+        respuesta.sendFile(`${__dirname}/views/idiomas.html`);
+    });
+
+    app.get('/aptitudes', (peticion, respuesta)=>{
+        respuesta.sendFile(`${__dirname}/views/aptitudes.html`);
+    });
+   
+
+
+      app.listen(3030);
+      console.log("funciona");
+
+
